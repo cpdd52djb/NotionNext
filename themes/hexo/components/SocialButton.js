7611e953bf970317784efe23a2cdc8c866187a4d
cpdd52djb <122464806+cpdd52djb@users.noreply.github.com>
2023-01-12 03:32:42 +0800

    Update SocialButton.js

--- themes/hexo/components/SocialButton.js
+++ themes/hexo/components/SocialButton.js
@@ -27,7 +27,7 @@ const SocialButton = () => {
       {BLOG.CONTACT_EMAIL && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${BLOG.CONTACT_EMAIL}`} >
         <i className='transform hover:scale-125 duration-150 fas fa-envelope dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>}
-      <a target='_blank' rel='noreferrer' title={'RSS'} href={'/feeds'} >
+      <a target='_blank' rel='noreferrer' title={'RSS'} href={''} >
         <i className='transform hover:scale-125 duration-150 fas fa-rss dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>
     </div>
